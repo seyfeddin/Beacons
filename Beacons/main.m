2bcfb11f31f5cff80321e2619f4b49cd74fd812c
Seyfeddin Bassarac <seyfoyun@gmail.com>
2014-05-16 13:36:00 +0200

    Initial Commit

---
+++ Beacons/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Beacons
+//
+//  Created by Seyfeddin Bassarac on 16/05/14.
+//  Copyright (c) 2014 ThreadCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BSAppDelegate class]));
+    }
+}
